7207b2f6981810d69f3b85ca26fadc8c7bbace8f
Amith P <amithanilp@gmail.com>
2026-01-05 21:07:00 +0530

    navbar comleted

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+const Navbar = () => {
+    const [open, setOpen] = useState(false)
+    return (
+        <>
+            <div className='w-full  bg-blue-600 '>
+                <div className='h-14 flex justify-between items-center px-4'>
+                    <div className='text-white text-xl cursor-pointer'>BlogsterZone</div>
+                    <div className='hidden md:flex gap-6'>
+                        <div className='nav-items'>Home</div>
+                        <div className='nav-items'>My Blogs</div>
+                        <div className='nav-items'>Login</div>
+                        <div className='nav-items'>Profile</div>
+                    </div>
+                
+                <button className="md:hidden text-white text-2xl" onClick={() => setOpen(!open)}>
+                    ☰
+                </button></div>
+                {open &&
+                    <div className='md:hidden flex flex-col gap-0.5 px-3'>
+                        <div className='nav-items'>Home</div>
+                        <div className='nav-items'>My Blogs</div>
+                        <div className='nav-items'>Login</div>
+                        <div className='nav-items'>Profile</div>
+                    </div>
+                }
+                
+            </div>
+        </>
+    )
+}
+
+export default Navbar
