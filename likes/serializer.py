e73d6d73cb349ab5ee16ebdc526b942b9ea69023
Amith P <amithanilp@gmail.com>
2026-01-05 20:08:28 +0530

    fontend

--- likes/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import Serializer
-from .models import Like
-
-class LikeSerializer(Serializer.ModelSerializer):
-    class Meta:
-        model=Like
-        fields=['id','user','blog','created_at']
-        read_only_fields=['user']
--- likes/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import toggle_like
-
-urlpatterns=[
-    path("blogs/<int:blog_id>/like/",toggle_like),
-]
