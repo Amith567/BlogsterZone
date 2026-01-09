fd3fb3418c9a3dde51274e1b20c38333a25475af
Amith P <amithanilp@gmail.com>
2026-01-10 00:45:32 +0530

    detail view completed

--- frontend/src/Pages/DetailBlog.jsx
+++ frontend/src/Pages/DetailBlog.jsx
@@ -1,25 +1,30 @@
-import React, { useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Navbar from '../Components/Navbar'
-
-
+import { DetailBlogView } from '../api/blog.api'
 const DetailBlog = () => {
-    const { id } = useParams()
+  
+  const { id } = useParams()
+  const [blog,setBlog]=useState([])
+  const [loading,setLoading]=useState(true)
   const [liked,setLiked]=useState(true)
   const [menuclicked,setMenuclicked]=useState(false)
-    const blog = {
-    "id": 101,
-    "author": "Amith Anil",
-    "title": "Understanding Zero-Knowledge Proofs ",
-    "content": "This artIn the modern digital era, privacy has become a critical concern as more systems rely on online authentication and data sharing. Traditional verification mechanisms often require users to disclose sensitive information, increasing the risk of data breaches and misuse. Zero-Knowledge Proofs address this challenge by offering a cryptographic method that allows one In the modern digital era, privacy has become a critical concern as more systems rely on online authentication and data sharing. Traditional verification mechanisms often require users to disclose sensitive information, increasing the risk of data breaches and misuse. Zero-Knowledge Proofs address this challenge by offering a cryptographic method that allows one party to prove the validity of a statement without revealing the underlying data itself.In the modern digital era, privacy has become a critical concern as more systems rely on online authentication and data sharing. Traditional verification mechanisms often require users to disclose sensitive information, increasing the risk of data breaches and misuse. Zero-Knowledge Proofs address this challenge by offering a cryptographic method that allows one party to prove the validity of a statement without revealing the underlying data itself.In the modern digital era, privacy has become a critical concern as more systems rely on online authentication and data sharing. Traditional verification mechanisms often require users to disclose sensitive information, increasing the risk of data breaches and misuse. Zero-Knowledge Proofs address this challenge by offering a cryptographic method that allows one party to prove the validity of a statement without revealing the underlying data itself.In the modern digital era, privacy has become a critical concern as more systems rely on online authentication and data sharing. Traditional verification mechanisms often require users to disclose sensitive information, increasing the risk of data breaches and misuse. Zero-Knowledge Proofs address this challenge by offering a cryptographic method that allows one party to prove the validity of a statement without revealing the underlying data itself.In the modern digital era, privacy has become a critical concern as more systems rely on online authentication and data sharing. Traditional verification mechanisms often require users to disclose sensitive information, increasing the risk of data breaches and misuse. Zero-Knowledge Proofs address this challenge by offering a cryptographic method that allows one party to prove the validity of a statement without revealing the underlying data itself.In the modern digital era, privacy has become a critical concern as more systems rely on online authentication and data sharing. Traditional verification mechanisms often require users to disclose sensitive information, increasing the risk of data breaches and misuse. Zero-Knowledge Proofs address this challenge by offering a cryptographic method that allows one party to prove the validity of a statement without revealing the underlying data itself.party to prove the validity of a statement without revealing the underlying data itself.icle explains the basics of ZKP and how it can be applied in secure voting systems.",
-    "visibility": "public",
-    "category": "Technology",
-    "category_id": 3,
-    "created_at": "2026-01-05T10:30:00Z",
-    "updated_at": "2026-01-05T12:15:00Z",
-    "like_count": 42,
-    "liked_by_user": true
-  }
+
+  useEffect(()=>{
+    const fetchdblog= async()=>{
+      try{
+        const res= await DetailBlogView(id)
+        setBlog(res.data)
+      }catch(err){
+        console.log('error',err)
+      }finally{
+        setLoading(false)
+      }
+    }
+    fetchdblog()
+  },[])
+
+console.log(blog)
   return (
     <>
       <Navbar />
@@ -28,7 +33,7 @@ const DetailBlog = () => {
         <div className='flex justify-between '>
           <div>
             <p className='text-2xl font-medium'>{blog.title}</p>
-            <div className='text-sm text-gray-800'>Author : {blog.author} | Published : {blog.created_at.slice(0, 10)} </div>
+            <div className='text-sm text-gray-800'>Author : {blog.author} | Published : {blog.created_at} </div>
           </div>
           <div className={`text-black text-2xl font-bold p-3 cursor-pointer ${menuclicked ? "bg-gray-200 rounded-full": ""} `} onClick={()=>setMenuclicked(!menuclicked)} >⋮</div>
         </div>{menuclicked && <div className='absolute right-5 top-0 mt-30 flex flex-col items-end  shadow-md z-2'>
@@ -39,10 +44,10 @@ const DetailBlog = () => {
 
         <div >
           <p className='p-2 '>{blog.content}</p>
-          <p className='text-xs text-blue-600'>last updated ({blog.updated_at.slice(0, 10)})</p>
+          <p className='text-xs text-blue-600'>last updated ({blog.updated_at})</p>
           <hr />
           <div className='flex justify-between pt-2'>
-            <div className='text-sm'>Tags : {blog.category}</div>
+            <div className='text-sm'>Tags : {blog.category?.name}</div>
             <div>
               <button className='mr-4'>comments</button>
               <button  onClick={()=>setLiked(!liked)} className={` px-2 py-1  rounded-md  ${liked ? 'text-black-700' : 'bg-blue-500 text-white'}`}>♡ {blog.like_count}</button>
--- frontend/src/api/blog.api.js
+++ frontend/src/api/blog.api.js
@@ -1,3 +1,6 @@
 import api from "./axios";
 
 export const ListBlogs=()=>api.get('/blogs/')
+export const DetailBlogView=(id)=>{
+    return api.get(`blogs/${id}/`);
+};
