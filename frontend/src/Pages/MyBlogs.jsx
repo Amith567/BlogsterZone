40447f4f5919952384544695907fd656cf98b552
Amith P <amithanilp@gmail.com>
2026-01-12 22:18:15 +0530

    user/blogs completed

--- frontend/src/Pages/MyBlogs.jsx
+++ frontend/src/Pages/MyBlogs.jsx
@@ -1,14 +1,31 @@
-
 import Navbar from '../Components/Navbar'
 import Blog from '../Components/Blog'
+import { useEffect, useState } from 'react'
+import { UserBlogs } from '../api/blog.api'
 
 const MyBlogs = () => {
+  const [blogs,setBlogs]=useState([])
+  const [error,setError]=useState('')
+  useEffect(()=>{
+    const loadblogs=async()=>{
+      try{
+        const res=await UserBlogs()
+        setBlogs(res.data)
+      }catch(err){
+        setError(err?.response?.data?.detail || 'Something went wrong!.')
+      }
+    }
+    loadblogs()
+  },[])
+
+  if (error){
+    return <p>{error}</p>
+  }
   return (
     <>
 <Navbar />
-{/* <Blog /> */}
-this is myblog page
-    </>
+{blogs.map((blog)=>(<Blog key={blog.id} blog={blog}/>))}
+</>
   )
 }
 
--- frontend/src/api/blog.api.js
+++ frontend/src/api/blog.api.js
@@ -1,9 +1,8 @@
 import api from "./axios";
 
-export const ListBlogs=()=>api.get('/blogs/')
+export const ListBlogs=()=>api.get('blogs/')
 export const DetailBlogView=(id)=>{
-    return api.get(`/blogs/${id}/`);
-};
-export const AddBlog=(data)=>api.post('/blogs/',data)
-
-export const LikeBlog=(id)=>api.post(`/blogs/${id}/like/`)
+    return api.get(`blogs/${id}/`);};
+export const AddBlog=(data)=>api.post('blogs/',data)
+export const LikeBlog=(id)=>api.post(`blogs/${id}/like/`)
+export const UserBlogs=()=>api.get('blogs/user/')
