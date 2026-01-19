bd1bbfaf6d2c58cf01f80f60e8306f2df3062dbd
Amith P <amithanilp@gmail.com>
2026-01-19 23:55:15 +0530

    components  completed

--- frontend/src/Components/Comments.jsx
+++ frontend/src/Components/Comments.jsx
@@ -3,15 +3,13 @@ import { Link } from "react-router-dom"
 const Comments = ({ comment }) => {
   return (
     <>
-
       <div className='my-3 border w-full rounded-md p-2'>
         <div className='flex flex-col'>
-          <p className='font-semibold underline cursor-pointer text-blue-900'><Link
-            to={`/${comment.commented_by_id}/profile`}
-            className="underline cursor-pointer text-blue-900"
-          >
-            {comment.commented_user}
-          </Link></p>
+          <p className='font-semibold underline cursor-pointer text-blue-900'>
+            <Link to={`/${comment.commented_by_id}/profile`} className="underline cursor-pointer text-blue-900">
+              {comment.commented_user}
+            </Link>
+          </p>
           <div className='text-xs text-gray-500'>{comment.created_at.slice(0, 10)} {comment.created_at.slice(11, 16)}</div>
         </div><hr />
         <div className='text-sm'>
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -3,9 +3,9 @@ import { Link, useLocation } from 'react-router-dom'
 
 const Navbar = () => {
     const [open, setOpen] = useState(false)
-    const location=useLocation()
-    const hideNewBlog= location.pathname==='/blog/add'
-    const isLogined=!!localStorage.getItem('access')
+    const location = useLocation()
+    const hideNewBlog = location.pathname === '/blog/add'
+    const isLogined = !!localStorage.getItem('access')
     return (
         <>
             <div className='w-full  bg-blue-800 sticky top-0 z-5'>
@@ -13,25 +13,24 @@ const Navbar = () => {
                     <div className='text-white text-2xl cursor-pointer' ><Link to='/'>BlogsterZone</Link></div>
                     <div className='hidden md:flex gap-6'>
                         <div className='nav-items'><Link to="/" className='w-full block'>Home</Link></div>
-                       {!hideNewBlog && isLogined &&<div className='nav-items' ><Link to="/blog/add" className='w-full block'>Create Blog</Link></div>}
-                        {isLogined &&<div className='nav-items' ><Link to="/user/blogs" className='w-full block'>My Blogs</Link></div>}
-                        {!isLogined &&(<div className='nav-items'><Link to='/login'>Login</Link></div>)}
-                       {isLogined && <div className='nav-items'><Link to='/profile' className='block w-full'>Profile</Link></div>}
+                        {!hideNewBlog && isLogined && <div className='nav-items' ><Link to="/blog/add" className='w-full block'>Create Blog</Link></div>}
+                        {isLogined && <div className='nav-items' ><Link to="/user/blogs" className='w-full block'>My Blogs</Link></div>}
+                        {!isLogined && (<div className='nav-items'><Link to='/login'>Login</Link></div>)}
+                        {isLogined && <div className='nav-items'><Link to='/profile' className='block w-full'>Profile</Link></div>}
                     </div>
-                
-                <button className="md:hidden text-white text-2xl" onClick={() => setOpen(!open)}>
-                    ☰
-                </button></div>
+
+                    <button className="md:hidden text-white text-2xl" onClick={() => setOpen(!open)}>
+                        ☰
+                    </button>
+                </div>
                 {open &&
                     <div className='md:hidden flex flex-col gap-0.5 px-3'>
                         <div className='nav-items'><Link to="/" className='w-full block'>Home</Link></div>
                         {!hideNewBlog && isLogined && <div className='nav-items'><Link to="/blog/add" className='w-full block'>Create Blog</Link></div>}
                         {isLogined && <div className='nav-items'><Link to="/user/blogs" className='w-full block'>My Blogs</Link></div>}
-                        {!isLogined&&<div className='nav-items'><Link to='/login'>Login</Link></div>}
-                        {isLogined&& <div className='nav-items'><Link to='/profile' className='block w-full'>Profile</Link></div>}
-                    </div>
-                }
-                
+                        {!isLogined && <div className='nav-items'><Link to='/login'>Login</Link></div>}
+                        {isLogined && <div className='nav-items'><Link to='/profile' className='block w-full'>Profile</Link></div>}
+                    </div>}
             </div>
         </>
     )
